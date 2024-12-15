7b9ff0816e95275bdf1fbed86850fae7a642dbad
arman <alarmanbejoy@gmail.com>
2024-12-15 15:21:07 +0600

    card+modal

--- src/ProductDetailsCard.jsx
+++ src/ProductDetailsCard.jsx
@@ -251,7 +251,7 @@ const ProductDetailsCard = () => {
           onClick={handleCheckoutClick}
           className="bg-blue-500 text-white px-4 py-2 rounded text-sm"
         >
-          Proceed to Checkout
+          Checkout
         </button>
       </div>
     </div>
